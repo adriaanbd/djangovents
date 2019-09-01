fc00e739af920af3d99814b5991c54fe023d25ce
Adriaan Beiertz <adriaanbd@gmail.com>
2019-09-01 15:28:02 -0500

    simple event show method

--- djangovents/events/views.py
+++ djangovents/events/views.py
@@ -3,9 +3,13 @@ from django.http import HttpResponse
 from .models import Event
 
 
-def index():
+def index(request):
     events = Event.objects.all()
     events_list = ''
     for event in events:
         events_list += f'<li>{event.name}</li>'
-    return HttpResponse(f'<ul>{events_list}</ul>')
+    return HttpResponse(f'<h1>Events</h1><ul>{events_list}</ul>')
+
+def show(request, event_id):
+    event = Event.objects.get(id=event_id)
+    return HttpResponse(f'<h1>{event.name}</h1>')

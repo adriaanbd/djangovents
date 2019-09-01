ff490b14bff884b88b7ce1d1a4464391437d8dea
Adriaan Beiertz <adriaanbd@gmail.com>
2019-09-01 16:10:21 -0500

    formatting 4 spaces

--- djangovents/events/models.py
+++ djangovents/events/models.py
@@ -4,14 +4,14 @@ from django.utils import timezone
 
 
 class Event(models.Model):
-  name = models.CharField('Event name', max_length=60)
-  event_date = models.DateField('Event date')
-  time_start = models.TimeField('Event start time')
-  time_end = models.TimeField('Event end time')
-  venue = models.CharField(max_length=120)
-  description = models.TextField(max_length=240)
-  creator = models.ForeignKey(User, on_delete=models.CASCADE)
-  date_created = models.DateTimeField(default=timezone.now)
+    name = models.CharField('Event name', max_length=60)
+    event_date = models.DateField('Event date')
+    time_start = models.TimeField('Event start time')
+    time_end = models.TimeField('Event end time')
+    venue = models.CharField(max_length=120)
+    description = models.TextField(max_length=240)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now)
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name

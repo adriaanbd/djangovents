8b5ff0cc7e4ecc544b37212107761863b279fb6f
Adriaan Beiertz <adriaanbd@gmail.com>
2019-09-01 15:14:16 -0500

    simple bullet point index of all events

--- djangovents/events/models.py
+++ djangovents/events/models.py
@@ -6,9 +6,12 @@ from django.utils import timezone
 class Event(models.Model):
   name = models.CharField('Event name', max_length=60)
   event_date = models.DateField('Event date')
-  event_time_start = models.TimeField('Event start time')
-  event_time_end = models.TimeField('Event end time')
+  time_start = models.TimeField('Event start time')
+  time_end = models.TimeField('Event end time')
   venue = models.CharField(max_length=120)
   description = models.TextField(max_length=240)
   creator = models.ForeignKey(User, on_delete=models.CASCADE)
   date_created = models.DateTimeField(default=timezone.now)
+
+  def __str__(self):
+    return self.name
--- djangovents/events/views.py
+++ djangovents/events/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Event
 
-# Create your views here.
+
+def index():
+    events = Event.objects.all()
+    events_list = ''
+    for event in events:
+        events_list += f'<li>{event.name}</li>'
+    return HttpResponse(f'<ul>{events_list}</ul>')

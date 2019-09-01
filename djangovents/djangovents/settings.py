c2924ee3e966baa8838d8d805e925bcd1d6e6005
Adriaan Beiertz <adriaanbd@gmail.com>
2019-09-01 12:32:47 -0500

    initial model with attributes

--- djangovents/djangovents/settings.py
+++ djangovents/djangovents/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'events.apps.EventsConfig',
+    'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

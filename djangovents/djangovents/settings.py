354b8c2e95763340b8adf84c8ecc07ccdeefd431
Adriaan Beiertz <adriaanbd@gmail.com>
2019-09-01 19:37:07 -0500

    add template and static dir path with base template

--- djangovents/djangovents/settings.py
+++ djangovents/djangovents/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'djangovents.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'djangovents/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'djangovents/static'),
+]
--- djangovents/events/views.py
+++ djangovents/events/views.py
@@ -4,11 +4,12 @@ from .models import Event
 
 
 def index(request):
-    events = Event.objects.all()
-    events_list = ''
-    for event in events:
-        events_list += f'<li>{event.name}</li>'
-    return HttpResponse(f'<h1>Events</h1><ul>{events_list}</ul>')
+    # events = Event.objects.all()
+    # events_list = ''
+    # for event in events:
+    #     events_list += f'<li>{event.name}</li>'
+    # return HttpResponse(f'<h1>Events</h1><ul>{events_list}</ul>')
+    return render(request, 'base.html')
 
 def show(request, event_id):
     event = Event.objects.get(id=event_id)
